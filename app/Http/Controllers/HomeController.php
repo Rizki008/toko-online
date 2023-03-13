a922f42a14a8a372b328a1bf0916fb27a84cd13e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-03-13 11:18:09 +0700

    menambahkan pemanggilan data product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,23 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\About;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Slider;
+use App\Models\Testimoni;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $sliders = Slider::all();
+        $categories = Category::all();
+        $testimonis = Testimoni::all();
+        $products = Product::skip(0)->take(8)->get();
+        return view('home.index', compact('sliders', 'categories', 'testimonis', 'products'));
     }
 
-    public function products()
+    public function products($id_subkategory)
     {
-        return view('home.products');
+        $products = Product::where('id_subkategori', $id_subkategory)->get();
+
+        return view('home.products', compact('products'));
     }
 
-    public function product()
+    public function product($id_product)
     {
-        return view('home.product');
+        $product = Product::find($id_product);
+        $latest_products = Product::orderByDesc('created_at')->offset(0)->limit(10)->get();
+        return view('home.product', compact('product', 'latest_products'));
     }
 
     public function cart()
@@ -38,12 +51,15 @@ class HomeController extends Controller
 
     public function about()
     {
-        return view('home.about');
+        $about = About::first();
+        $testimonis = Testimoni::all();
+        return view('home.about', compact('about', 'testimonis'));
     }
 
     public function contact()
     {
-        return view('home.contact');
+        $about = About::first();
+        return view('home.contact', compact('about'));
     }
 
     public function faq()
